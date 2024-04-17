6d3182eb21d3ed08945e7c8c6d257cd00d8a3115
rezekiz <149320958+rezekiz@users.noreply.github.com>
2024-04-17 17:08:44 +0100

    Create evol_alg.py

---
+++ algoritmos_evolucionarios/tests/evol_alg.py
@@ -0,0 +1,16 @@
+"""
+Funcionalidades chave de algoritmos evolucionários:
+
+* criação de população inicial de forma aleatória
+* avaliação de população inicial
+* avaliação de individuos
+* seleção de progenitores
+* criação de novas soluções (mutações, cruzamento)
+* determinar nova população a partir da população anterior e novas soluções
+
+
+
+
+"""
+
+import unittest
