ca6ad7a70b9a482fa632c231c57d2760c4bbf7a9
Rui Sousa <149320958+rezekiz@users.noreply.github.com>
2024-04-29 18:43:17 +0100

    Update main.py

--- grafos/main.py
+++ grafos/main.py
@@ -3,3 +3,30 @@ Criação de módulo de grafos
 Utilização de graphviz para visualizar
 A testagem tem de considerar a adição de nós, vértices e ligações
 """
+
+
+import graphviz
+
+class Graph(graph):
+  def __init__(self):
+    if graph is None:
+      self.g = {}
+    else:
+      self.g = g
+
+  def add_node(self, node):
+    if node not in self:
+      self[node] = []
+
+  def add_branch(self, origin, destination):
+    if origin not in self.keys():
+      add_node(self, origin)
+    elif destination not in self.keys():
+      add_node(self, destination)
+    elif destination not in self[origin]:
+      self[origin].append(destination)
+      
+    
+
+
+
