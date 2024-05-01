dcb59cb938fbe76c10676785ea1670e381eaf1bb
Rui S <contact@ruisousa.me>
2024-05-01 17:54:23 +0100

    grafos base

--- grafos/main.py
+++ grafos/main.py
@@ -2,14 +2,12 @@
 Criação de módulo de grafos
 Utilização de graphviz para visualizar
 A testagem tem de considerar a adição de nós, vértices e ligações
-"""
 
-
-"""
 Autor: Rui Sousa
 """
 
 import graphviz
+import pandas as pd
 
 # Métodos estáticos para usar graphviz para renderizar
 
@@ -27,8 +25,15 @@ class Graph():
   def __init__(self, g = None):
     if g is None:
       self.g = {}
+    
     else:
       self.g = g
+
+  """
+  Secção de métodos e operadores básicos de grafos:
+    - Adição e remoção de nós
+    - Adição e remoção de ramificações
+  """
 
   def add_node(self, node):
     if node not in self.g:
@@ -90,24 +95,51 @@ class Graph():
       origin, destinations = edge.split('->')
       for destination in destinations.split(','):
         self.g[origin].remove(destination)
-      
-
-
-
-    
-
-
-
   
+  """
+  Secção de métodos para informação básica de grafos:
+    - Encontrar sucessores, antecessores e adjacentes
+    - Grau (in/out) de cada nó
+    - Matriz de adjacência
+  """
 
+  def get_successors(self, node):
+    return list(self.g[node])
+    
+  def get_predecessors(self, node):
 
+    predecessors = []
 
+    for key, value in self.g.items():
+      if node in value:
+        predecessors.append(key)
+    
+    return predecessors
 
+  def get_adjacents(self, node):
 
+    predecessors = self.get_predecessors(node)
+    successors = self.get_successors(node)
+    adjacents = list(set(predecessors + successors))
+    return adjacents
 
+  def adj_matrix(self):
+    matrix = [[] for node in self.g.keys()]
+    nodes = list(self.g.keys())
+    iter = 0
 
+    for node in self.g.keys():
+      for _ in nodes:
+        if node in self.get_adjacents(_):
+          matrix[iter].append(1)
+        else:
+          matrix[iter].append(0)
+      iter += 1
 
+    matrix = pd.DataFrame(
+        matrix,
+        index = [node for node in self.g.keys()],
+        columns = [node for node in self.g.keys()]
+    )
 
-
-
-
+    return matrix
