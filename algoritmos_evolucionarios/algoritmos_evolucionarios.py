1485b8de436397edba3b4708e1cdc1f098d00f28
Mariana <oliveira1mariana@hotmail.com>
2024-05-20 14:20:18 +0100

    Alg.evol

--- algoritmos_evolucionarios/algoritmos_evolucionarios.py
+++ algoritmos_evolucionarios/algoritmos_evolucionarios.py
@@ -46,14 +46,36 @@ class Individuos:
         for i in range(size):
             self.genome.append(random.randint(self.lim_inf, self.lim_sup))
 
-    pass
+        pass
+
+    # Inicialização de operador de mutação que altera um único gene
+    def mutation(self):
+        """
+            Altera um gene aleatório do genoma do indivíduo.
+
+            Parâmetros:
+                None
+
+            Retorno:
+                None
+        """
+
+        tamanho = len(self.genome)
+        pos_gene = random.randint(0, tamanho - 1)
+
+        if self.genome[pos_gene] != 0:
+            self.genome[pos_gene] = 0
+        else:
+            self.genome[pos_gene] = 1
+
+        pass
 
 
 # Classe população
 
 class Populacao:
 
-    def __init__(self, pop_size: int, indiv_size: int, indivs: int):
+    def __init__(self, pop_size: int, indiv_size: int, indivs: list = None):
         """
             Inicializa um novo objeto da classe populacao.
 
@@ -76,10 +98,64 @@ class Populacao:
 
     # Inicialização aleatória da população
     def init_aleat_pop(self):
+        """
+            Inicializa um novo objeto da classe populacao com valores aleatórios.
+
+            Parâmetros:
+                None
+
+            Retorno:
+                None
+        """
 
         self.indivs = []
 
         for i in range(self.pop_size):
-            self.indivs.append(Individuos(self.indiv_size))
+            self.indivs.append(Individuos(self.indiv_size, [], 1, 0))
+
+        pass
+
+    # Retorna o indivíduo da população com o parâmetro index
+    def get_indiv(self, index: int):
+        """
+            Retorna o indivíduo da população com o parâmetro index.
+
+            Parâmetros:
+                index (int): O indice do indivíduo da população.
+
+            Retorno:
+                Individuos: O indivíduo da população.
+        """
+        return self.indivs[index]
+
+    # Retorna lista de aptidões de todos os indivíduos da população
+    def get_fitnesses(self):
+        """
+            Retorna a lista de aptidões de todos os indivíduos da população.
+
+            Parâmetros:
+                None
+
+            Retorno:
+                list: A lista de aptidões de todos os indivíduos da população.
+        """
+        fitnesses = []
+        for indiv in self.indivs:
+            fitnesses.append(indiv.fitness)
+        return fitnesses
+
+    # Seleciona o melhor score de entre os indivíduos da população
+    def get_best_indiv(self):
+        """
+            Retorna o melhor indivíduo da população.
+
+            Parâmetros:
+                None
+
+            Retorno:
+                Individuos: O melhor indivíduo da população.
+        """
+
+        return max(self.indivs, key=lambda x: x.fitness)
 
     pass
