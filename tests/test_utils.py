b1e5313ede3e3a68bbb5fe564ade1408ee1eb55b
Rui S <contact@ruisousa.me>
2024-06-10 18:20:21 +0100

    fixing tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,8 @@
+# Add parent dir to path to ensure module is found
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import unittest
 from utils import pwm
 
