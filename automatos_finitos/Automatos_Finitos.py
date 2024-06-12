f06ec02a564fb58a19d5d65a9446a4a4b66cf5c3
Carlos Gomes <149185177+CarlosGomes00@users.noreply.github.com>
2024-06-12 17:13:27 +0100

    Update Automatos_Finitos.py

--- automatos_finitos/Automatos_Finitos.py
+++ automatos_finitos/Automatos_Finitos.py
@@ -1,3 +1,8 @@
+"""
+Código base retirado dos powerpoints das aulas (da autoria de Rui Mendes) com pequenas alterações.
+Utilização do stack overflow e de LLMs (GPT 3.5 e Gemini 1.5) para algumas correções
+"""
+
 if __name__ == "__main__":
 
     from Auxiliares import sobreposicao
