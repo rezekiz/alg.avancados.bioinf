2764976da9f74d13066222f3d62bf835749747be
Mariana <oliveira1mariana@hotmail.com>
2024-06-12 23:39:23 +0100

    update

--- tests/test_boyer-moore.py
+++ tests/test_boyer-moore.py
@@ -8,28 +8,20 @@ import unittest
 from boyer_moore.BoyerMoore import BoyerMoore
 
 class TestBoyerMoore(unittest.TestCase):
-
-    def test_search_pattern(self):
-        # Teste básico
-        bm = BoyerMoore("ACTG", "ACCA")
-        result = bm.search_pattern("ATAGAACCAATGAACCATGATGAACCATGGATACCCAACCACC")
-        self.assertEqual(result, [5, 13, 22, 36])
-
-        # Teste com um padrão diferente
-        bm = BoyerMoore("ACTG", "TG")
-        result = bm.search_pattern("ATAGAACCAATGAACCATGATGAACCATGGATACCCAACCACC")
-        self.assertEqual(result, [1, 14, 22, 26, 33])
-
+    
+    def test_search_pattern_2(self):
         # Teste com um padrão que não está presente
         bm = BoyerMoore("ACTG", "GTC")
         result = bm.search_pattern("ATAGAACCAATGAACCATGATGAACCATGGATACCCAACCACC")
         self.assertEqual(result, [])
-
+    
+    def test_search_pattern_3(self):
         # Teste com sequência vazia
         bm = BoyerMoore("ACTG", "ACCA")
         result = bm.search_pattern("")
         self.assertEqual(result, [])
 
+    def test_search_pattern_4(self):
         # Teste com padrão vazio
         bm = BoyerMoore("ACTG", "")
         result = bm.search_pattern("ATAGAACCAATGAACCATGATGAACCATGGATACCCAACCACC")
